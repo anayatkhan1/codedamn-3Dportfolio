87b5b4cb1cf23804513c692e67b35db3111abd33
Anayat Khan <anayat0khan@gmail.com>
2024-01-19 18:38:44 +0530

    Codedamn-3DPortfolio

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const App = () => {
+  return (
+    <Router>
+        <div className="relative z-0 bg-primary">
+            <div className="bg-cover bg-hero-pattern">
+                <Navbar />
+                <Hero />
+            </div>
+            <About />
+            <Experience /> 
+            <Tech />
+            <Works />
+            <Feedbacks />
+            <div className="relative z-0">
+                <Contact />
+                <StarsCanvas />
+            </div>
+        </div>
+
+    </Router> 
+  )
+}
+
+export default App
